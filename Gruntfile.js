da02aafd0729479f72eba7eff0dcc3d83d1ef99a
Sergio A Guzman <sagg629@gmail.com>
2015-08-22 12:24:51 -0500

    Reformatted code.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,19 @@
 var pkgjson = require('./package.json');
 
 module.exports = function (grunt) {
-  grunt.initConfig({
-    uglify: {
-      core: {
-        files: [{
-          expand: true,
-          cwd: 'src/main/resources/',
-          src: 'static/**/*.js',
-          dest: "target/classes",
-          ext: '.min.js'
-        }]
-      }
-    }
-  });
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.registerTask('default', ['uglify']);
+    grunt.initConfig({
+        uglify: {
+            core: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/main/resources/',
+                    src: 'static/**/*.js',
+                    dest: "target/classes",
+                    ext: '.min.js'
+                }]
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['uglify']);
 };
