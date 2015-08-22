03f14c3f878715bef78526a29f1dc0c0a9eae460
Sergio A Guzman <sagg629@gmail.com>
2015-08-22 12:17:45 -0500

    Modified routes, reformatted code.

--- src/main/java/cnagel/springnode/Application.java
+++ src/main/java/cnagel/springnode/Application.java
@@ -1,7 +1,7 @@
 package cnagel.springnode;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,13 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-	
-	@RequestMapping("/")
-	public String index() {
-		return "index";
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
+    @RequestMapping("/")
+    public String index() {
+        return "chat";
+    }
 }
